bb9408ad1d4d1d21caefab9944297267aa9d0b96
Clarke Fry <clarkefry@gmail.com>
2017-12-20 12:36:42 -0600

    function js and html files

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [{
+  name: "John Lennon",
+  photo: "https://img.discogs.com/J19UWxyuyProuiwhaQSfzeDUchM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-46481-1286626601.jpeg.jpg",
+  scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+}, {
+  name: "Bob Marley",
+  photo: "http://www.bobmarley.com/wp-content/uploads/2013/10/bob_marley2.jpg",
+  scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+}, {
+  name: "Jimmy Fallon",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Jimmy_Fallon%2C_Montclair_Film_Festival%2C_2013.jpg",
+  scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+},{
+  name: "Salvador Perez",
+  photo: "http://mlb.mlb.com/mlb/images/players/head_shot/521692.jpg",
+  scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+}, {
+  name: "Eric Church",
+  photo: "https://cdn-a.stagebloc.com/production/photos/2912/large/20160104_163825_2912_778864.jpeg",
+  scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+  name: "Jack Nicholson",
+  photo: "http://i.dailymail.co.uk/i/pix/2011/01/26/article-1350653-0CD4A9C3000005DC-358_306x423.jpg",
+  scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+  name: "Jerry Seinfeld",
+  photo: "http://viewthevibe.com/wp-content/uploads/2016/11/LAm8cB1b.jpeg",
+  scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+}];
+
+module.exports = friends;
